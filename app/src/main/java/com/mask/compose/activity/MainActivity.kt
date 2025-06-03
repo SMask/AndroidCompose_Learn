4efa9236b489fedb4487b24ef809090615854b33
Mask <869370440@qq.com>
2025-06-03 21:39:37 +0800

    封装 Button

--- app/src/main/java/com/mask/compose/activity/MainActivity.kt
+++ app/src/main/java/com/mask/compose/activity/MainActivity.kt
@@ -7,20 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mask.compose.R
+import com.mask.compose.ui.ButtonNormal
 import com.mask.compose.ui.theme.AndroidCompose_DemoTheme
 import com.mask.compose.ui.theme.Dimen
 import com.mask.compose.utils.LogUtils
@@ -73,55 +70,40 @@ fun MainLayout(modifier: Modifier = Modifier) {
         modifier = modifier
             .verticalScroll(rememberScrollState())
     ) {
-        ActivityButton(
+        ButtonNormal(
             modifier = buttonModifier(false),
             textResId = R.string.title_activity_common_view,
             onClick = {
                 CommonViewActivity.startActivity(context)
             }
         )
-        ActivityButton(
+        ButtonNormal(
             modifier = buttonModifier(),
             textResId = R.string.title_activity_draggable_view,
             onClick = {
                 DraggableViewActivity.startActivity(context)
             }
         )
-        ActivityButton(
+        ButtonNormal(
             modifier = buttonModifier(),
             textResId = R.string.title_activity_state_view_model,
             onClick = {
                 StateViewModelActivity.startActivity(context)
             }
         )
-        ActivityButton(
+        ButtonNormal(
             modifier = buttonModifier(),
             textResId = R.string.title_activity_list,
             onClick = {
                 ListActivity.startActivity(context)
             }
         )
-        ActivityButton(
+        ButtonNormal(
             modifier = buttonModifier(),
             textResId = R.string.title_activity_animation,
             onClick = {
                 AnimationActivity.startActivity(context)
             }
-        )
-    }
-}
-
-@Composable
-fun ActivityButton(textResId: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    LogUtils.i("ActivityButton ${stringResource(textResId)}")
-
-    Button(
-        modifier = modifier
-            .height(Dimen.buttonHeight),
-        onClick = onClick
-    ) {
-        Text(
-            text = stringResource(textResId)
         )
     }
 }
